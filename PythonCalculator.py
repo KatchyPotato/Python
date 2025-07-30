c203c128e3e0849975c2db019d74c0ed2210f681
Katchy <jgaud486@evit.edu>
2025-07-30 15:56:42 -0700

    Update PythonCalculator.py

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -3,14 +3,23 @@ arithmetic = input("What arithmatic would you like to use?")
 number2 = int(input("Input second number:"))
 
 if arithmetic == "+":
-    print(number1 + number2)
+    calculation = (number1 + number2)
+    print(calculation)
 
 elif arithmetic == "-":
+    calculation = (number1 - number2)
     print(number1 - number2)
 
 elif arithmetic == "*":
+    calculation = (number1 * number2)
     print(number1 * number2)
 
 elif arithmetic == "/":
+    calculation = (number1 /number2)
     print(number1 / number2)
+
+end_message = "Calculation complete. Your calculation: {0} {1} {2} = {3}"
+print(end_message.format(number1, arithmetic, number2, calculation))
+
+try_again = input("Would you like to try again? (Yes or No)")
 
