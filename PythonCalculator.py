a6e86eca40f8b53e9275ffeca9ee45d67e46730d
Katchy <jgaud486@evit.edu>
2025-07-29 17:28:11 -0700

    Update PythonCalculator.py

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -1,4 +1,16 @@
-number1 = int(input("Input first number"))
-arithmatic = input("What arithmatic would you like to use?")
-number2 = int(input("Input second number"))
+number1 = int(input("Input first number:"))
+arithmetic = input("What arithmatic would you like to use?")
+number2 = int(input("Input second number:"))
+
+if arithmetic == "+":
+    print(number1 + number2)
+
+elif arithmetic == "-":
+    print(number1 - number2)
+
+elif arithmetic == "*":
+    print(number1 * number2)
+
+elif arithmetic == "/":
+    print(number1 / number2)
 
