f8b643c14332481feaf3b348900ae8e335ca043b
Katchy <jgaud486@evit.edu>
2025-07-31 18:22:07 -0700

    Update PythonCalculator.py

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -1,5 +1,5 @@
 number1 = int(input("Input first number:"))
-arithmetic = input("What arithmatic would you like to use?")
+arithmetic = input("Input arithmetic:")
 number2 = int(input("Input second number:"))
 
 if arithmetic == "+":
@@ -22,4 +22,34 @@ end_message = "Calculation complete. Your calculation: {0} {1} {2} = {3}"
 print(end_message.format(number1, arithmetic, number2, calculation))
 
 try_again = input("Would you like to try again? (Yes or No)")
+
+if try_again == "No":
+    print("CALCULATOR TERMINATED")
+elif try_again == "Yes":
+
+    number1 = int(input("Input first number:"))
+arithmetic = input("Input arithmetic:")
+number2 = int(input("Input second number:"))
+
+if arithmetic == "+":
+    calculation = (number1 + number2)
+    print(calculation)
+
+elif arithmetic == "-":
+    calculation = (number1 - number2)
+    print(number1 - number2)
+
+elif arithmetic == "*":
+    calculation = (number1 * number2)
+    print(number1 * number2)
+
+elif arithmetic == "/":
+    calculation = (number1 /number2)
+    print(number1 / number2)
+
+end_message = "Calculation complete. Your calculation: {0} {1} {2} = {3}"
+print(end_message.format(number1, arithmetic, number2, calculation))
+
+
+
 
