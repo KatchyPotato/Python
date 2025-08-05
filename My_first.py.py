18f6e18c591ddb1b6d32e73939bfdc54fb410be5
Katchy <jgaud486@evit.edu>
2025-08-05 09:07:14 -0700

    Update My_first.py.py

--- My_first.py.py
+++ My_first.py.py
@@ -1,1 +1,8 @@
-print ("Hello World")
+name = input("[INPUT NAME]")
+intro = ("Hello" + name + "! I am the Inteligence")
+print(intro)
+print("Say... want to play little guessing game?")
+answere = input("[YES OR NO]")
+
+if answere == "yes":
+    print("Wonderful! Let's get started")
