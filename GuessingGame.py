82078b251a56864406dd0b66c6d4438244611541
Katchy <jgaud486@evit.edu>
2025-08-07 10:59:37 -0700

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,5 +1,7 @@
 import random
 
+#A simple guessing game which uses if and if else statments to compare user input with a randomly selected number
+
 print("Let's play a guessing game. You have three attempts")
 number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 correct_number = random.choice(number_list)
@@ -32,28 +34,24 @@ elif attempt1 > correct_number:
 
                 
 
-        elif attempt2 < correct_number:
-            print("\nIncorrect! To low. Try again.")
-            attempt3 = int(input("\nInput third guess: (1-10"))
+    elif attempt2 < correct_number:
+        print("\nIncorrect! To low. Try again.")
+        attempt3 = int(input("\nInput third guess: (1-10"))
 
-            if attempt3 == correct_number:
-                print(f"\nYou won! The correct number was: {correct_number}")
+        if attempt3 == correct_number:
+            print(f"\nYou won! The correct number was: {correct_number}")
                     
-            elif attempt3 > correct_number:
-                print(f"\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
+        elif attempt3 > correct_number:
+            print(f"\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
 
-            elif attempt3 < correct_number:
-                print(f"\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
+        elif attempt3 < correct_number:
+            print(f"\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
 
 
                 
                 
 
                
-
-
-
-
 elif attempt1 < correct_number:
     print("\nIncorrect! To low. Try again.")
     attempt2 = int(input("\nInput second guess: (1-10)"))
