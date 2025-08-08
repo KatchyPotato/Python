c290646ae30a4f0a47be898451c82b26ddac7b0c
Katchy <jgaud486@evit.edu>
2025-08-08 14:52:16 -0700

    Update GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,90 +1,58 @@
 import random
 
-#A simple guessing game which uses if and if else statments to compare user input with a randomly selected number
+print("Let's play a guessing game. You have three attempts.")
 
-print("Let's play a guessing game. You have three attempts")
 number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 correct_number = random.choice(number_list)
 
-attempt1 = int(input("\nInput first guess: (1-10)"))
-if attempt1 == correct_number:
-    print(f"\nYou won! The correct number was: {correct_number}")
+attempts = 3
 
-elif attempt1 > correct_number:
-    print("\nIncorrect! To high. Try again.")
-    attempt2 = int(input("\nInput second guess: (1-10)"))
+while attempts > 0:
 
-    if attempt2 == correct_number:
-        print(f"\nYou won! The correct number was: {correct_number}")
+    if attempts == 3:
+        guess = int(input("\nInput first guess: (1-10)"))
 
-    elif attempt2 > correct_number:
-        print("\nIncorrect! To high. Try again.")
-        attempt3 = int(input("\nInput third guess: (1-10"))
-
-        if attempt3 == correct_number:
+        if guess == correct_number:
             print(f"\nYou won! The correct number was: {correct_number}")
 
-        elif attempt3 > correct_number:
-            print(f"\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
+            attempts = 0
 
-        elif attempt3 < correct_number:
-            print(f"\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
+        elif guess > correct_number:
+            print("\nIncorrect! To high. Try again.")
 
-
-
-                
-
-    elif attempt2 < correct_number:
-        print("\nIncorrect! To low. Try again.")
-        attempt3 = int(input("\nInput third guess: (1-10"))
-
-        if attempt3 == correct_number:
-            print(f"\nYou won! The correct number was: {correct_number}")
-                    
-        elif attempt3 > correct_number:
-            print(f"\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
-
-        elif attempt3 < correct_number:
-            print(f"\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
-
-
-                
-                
-
-               
-elif attempt1 < correct_number:
-    print("\nIncorrect! To low. Try again.")
-    attempt2 = int(input("\nInput second guess: (1-10)"))
-
-    if attempt2 == correct_number:
-        print(f"\nYou won! The correct number was: {correct_number}")
-
-    elif attempt2 > correct_number:
-        print("\nIncorrect! To high. Try again.")
-        attempt3 = int(input("\nInput third guess: (1-10"))
-        if attempt3 == correct_number:
-            print(f"\nYou won! The correct number was: {correct_number}")
-
-        elif attempt3 > correct_number:
-            print("f\nIncorrect! Game over. You ran out of guesses. The number was: {correct_number}")
-
-
-
-
-        elif attempt2 < correct_number:
+        elif guess < correct_number:
             print("\nIncorrect! To low. Try again.")
-            attempt3 = int(input("\nInput third guess: (1-10"))
-            if attempt3 == correct_number:
-                print(f"\nYou won! The correct number was: {correct_number}")
 
-            elif attempt3 > correct_number:
-                print("f\nIncorrect! Game over. You ran out of guesses. The number was:{correct_number}")
-            else:
-                print("f\nIncorrect! Game over. You ran out of guesses. The number was:{correct_number}")
+    elif attempts == 2:
+        guess = int(input("\nInput second guess: (1-10)"))
 
+        if guess == correct_number:
+            print(f"\nYou won! The correct number was: {correct_number}")
 
+            attempts = 0
 
+        elif guess > correct_number:
+            print("\nIncorrect! To high. Try again.")
 
+        elif guess < correct_number:
+            print("\nIncorrect! To low. Try again.")
 
+    elif attempts == 1:
+        guess = int(input("\nInput third guess: (1-10)"))
+
+        if guess == correct_number:
+            print(f"\nYou won! The correct number was: {correct_number}")
+
+            attempts = 0
+
+        elif guess > correct_number:
             
-            
+            print(f"\nGame over! You ran out of guesses. The correct number was: {correct_number}")
+                        
+        else:
+            print(f"\nGame over! You ran out of guesses. The correct number was: {correct_number}")
+                        
+
+    attempts -= 1
+                        
+                        
